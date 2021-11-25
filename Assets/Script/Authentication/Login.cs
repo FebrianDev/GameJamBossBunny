14b1d5f3b381fb45560d43997abf2a3a9c2e47cf
Febrian <59677015+FebrianDev@users.noreply.github.com>
2021-11-25 16:26:09 +0700

    add login & main menu

--- Assets/Script/Authentication/Login.cs
+++ Assets/Script/Authentication/Login.cs
@@ -1,18 +1,64 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Firebase.Database;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Login : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private string nameExist, pwdExist;
+    [SerializeField] private InputField name, pwd;
+    [SerializeField] private Text err;
+
+    public void Enter()
     {
-        
+        FirebaseDatabase.DefaultInstance
+            .GetReference("Users")
+            .ValueChanged += GetUserExist;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GetUserExist(object sender2, ValueChangedEventArgs e2)
     {
-        
+        if (e2.DatabaseError != null)
+        {
+            Debug.LogError(e2.DatabaseError.Message);
+        }
+
+        if (e2.Snapshot != null && e2.Snapshot.ChildrenCount > 0)
+        {
+            foreach (var childSnapshot in e2.Snapshot.Children)
+            {
+                nameExist = childSnapshot.Child("name").Value.ToString();
+                pwdExist = childSnapshot.Child("password").Value.ToString();
+
+                Debug.Log(nameExist);
+                Debug.Log(name.text);
+                if (name.text == nameExist)
+                {
+                    if (pwd.text == pwdExist)
+                    {
+                        err.text = "";
+                        PlayerPrefs.SetString(Constant.KEY_NAME, name.text);
+                        SceneManager.LoadScene("MainMenu");
+                    }
+                    else
+                    {
+                        var message = "Password is Wrong!";
+                        err.text = message;
+                        print(message);
+                    }
+                }
+                else
+                {
+                    var message = "Username not exist!";
+                    err.text = message;
+                    print(message);
+                }
+            }
+        }
+    }
+
+    public void GotoRegister()
+    {
+        SceneManager.LoadScene("Register");
     }
 }
---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,74 @@
+﻿using Firebase.Database;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Text nameText;
+    private string name;
+    private DataUser user;
+
+    void Start()
+    {
+        name = PlayerPrefs.GetString(Constant.KEY_NAME);
+        FirebaseDatabase.DefaultInstance
+            .GetReference("Users")
+            .ValueChanged += GetUser;
+    }
+
+    private void GetUser(object sender2, ValueChangedEventArgs e2)
+    {
+        if (e2.DatabaseError != null)
+        {
+            Debug.LogError(e2.DatabaseError.Message);
+        }
+
+        if (e2.Snapshot != null && e2.Snapshot.ChildrenCount > 0)
+        {
+            foreach (var childSnapshot in e2.Snapshot.Children)
+            {
+                var n = childSnapshot.Child("name").Value.ToString();
+                if (n == name)
+                {
+                    SetName(name);
+                }
+            }
+        }
+    }
+
+    private void SetName(string name)
+    {
+        nameText.text = "Welcome " + name;
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Scene-0_MainMenu");
+    }
+
+    public void EditProfile()
+    {
+        SceneManager.LoadScene("EditProfile");
+    }
+
+    public void Leaderboard()
+    {
+        SceneManager.LoadScene("Leaderboard");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credit");
+    }
+
+    public void Shop()
+    {
+        SceneManager.LoadScene("Shop");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
