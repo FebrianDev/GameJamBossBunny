589cc540c8d9ce6e64c5e742cac5abbf9983a2dc
Febrian <59677015+FebrianDev@users.noreply.github.com>
2021-11-28 11:34:03 +0700

    add script splash screen

---
+++ Assets/Script/SplashScreen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    private string keyName;
+    void Start()
+    {
+        keyName = PlayerPrefs.GetString(Constant.KEY_NAME);
+
+        StartCoroutine("StartSplashCreen");
+    }
+
+    IEnumerator StartSplashCreen()
+    {
+        yield return new WaitForSeconds(3);
+        if (keyName.Equals(""))
+        {
+            SceneManager.LoadScene("Register");
+        }
+        else
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
